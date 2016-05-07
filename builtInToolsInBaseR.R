eb866bd33ee7caadf62dc93907abe7930fdf0324
pseudorational <vishal.lala@gmail.com>
2016-05-06 21:20:58 -0400

    upload_05_06_2016

---
+++ builtInToolsInBaseR.R
@@ -0,0 +1,8 @@
+data() # built in data sets in R
+ls()   # list of all objects
+demo() # various demos
+search() # builtin packages
+history() # list of commands given
+dir()     # list of files in working directory
+getwd()   # get working directory
+builtins() # builtin objects in R
